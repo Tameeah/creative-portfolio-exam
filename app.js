434ab32201288287e0cec50942127f1e8c7ba2ce
Tameeah <tameeah.padayachy@gmail.com>
2026-05-05 21:36:50 +0200

    Artworks page - try to add flip card
    Tried to add flip card in artworks html, css and js.
    The card flip does not work yet. Not flipping around.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.flip-card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', function() {
+            // Toggle the 'is-flipped' class on click
+            this.classList.toggle('is-flipped');
+        });
+    });
+});
