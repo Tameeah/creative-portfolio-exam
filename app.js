71e99cbdee9f71e29293ea8d8055f626e5f339fb
Tameeah <tameeah.padayachy@gmail.com>
2026-05-15 19:25:12 +0200

    Experience Page added content
    Added Year Cards to the Experience Page.
    Added the CSS and HTML.
    Added JS so that when the user clicks the year card it will pop up and bring up more information about that year's project

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ cards.forEach((card) => {
     });
 });
 
+function openProject(year, overview, images) {
+    // 1. Set the Title and Text
+    document.getElementById('popupTitle').innerText = year;
+    document.getElementById('popupOverview').innerText = overview;
+
+    // 2. Set the Images
+    document.getElementById('pImg1').src = images[0];
+    document.getElementById('pImg2').src = images[1];
+    document.getElementById('pImg3').src = images[2];
+
+    // 3. Show the pop-up
+    document.getElementById('projectPopup').style.display = 'block';
+}
+
+function closeProject() {
+    // Hide the pop-up
+    document.getElementById('projectPopup').style.display = 'none';
+}
+
+
